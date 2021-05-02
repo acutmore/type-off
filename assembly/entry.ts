9052b9cc9a8be7aca62ea1a33d2b2d397dac3f70
Ashley Claymore <acutmore@users.noreply.github.com>
2021-05-02 21:05:35 +0100

    handle input longer than 2^16

--- assembly/entry.ts
+++ assembly/entry.ts
@@ -2,10 +2,17 @@ import 'wasi';
 import { transform } from "./index";
 
 // AssemblyScript WASI wrapper: stdout(transform(stdin()))
-const bufferIn = new ArrayBuffer(2 ** 13);
+const bufferIn = new ArrayBuffer(2 ** 22);
 const view = new DataView(bufferIn);
-const count = process.stdin.read(bufferIn, 0);
-view.setUint8(count + 1, 0);
+let offset = 0;
+for (;;) {
+    const count = process.stdin.read(bufferIn, offset);
+    if (count === 0) {
+        break;
+    }
+    offset += count;
+}
+view.setUint8(offset, 0);
 const code = String.UTF8.decode(bufferIn, true);
 const out = transform(code);
 process.stdout.write(out.code);
